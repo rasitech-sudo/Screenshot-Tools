3e88eeecc0b1f3cb31db27198049cec353ab3f7b
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-08-05 18:19:16 +0000

    Update screenshot.php

--- screenshot.php
+++ screenshot.php
@@ -2,9 +2,7 @@
 $a = $_POST['url'];
 $url = "https://api.apiflash.com/v1/urltoimage?access_key=c2b0969708484176814e87896cc231f6&full_page=true&url=$a";
 if (!empty($a)) {
-header('Content-Type: image/jpeg');
-    header('Content-Length: ' . filesize($url));
-    echo file_get_contents($url);
+header("location: $url");
 } else {
     header("location:https://rasi-tech.herokuapp.com/");
 }
