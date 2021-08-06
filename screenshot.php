492e7cd963c35faffaf11f193a9a3bf9a3b843f6
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-08-06 01:37:23 +0000

    Update screenshot.php

--- screenshot.php
+++ screenshot.php
@@ -2,15 +2,9 @@
 $a = $_POST['url'];
 $url = "https://api.apiflash.com/v1/urltoimage?access_key=c2b0969708484176814e87896cc231f6&full_page=true&url=$a";
 if (!empty($a)) {
-// Read image path, convert to base64 encoding
-$imageData = base64_encode(file_get_contents($url));
-
-// Format the image SRC:  data:{mime};base64,{data};
-$src = 'data: '.mime_content_type($url).';base64,'.$imageData;
-
-// Echo out a sample image
-    header('Content-Type: image/jpeg');
-    echo $src;
+    header('Content-type: image/jpeg');
+$image = file_get_contents($url);
+echo $image;
 } else {
     header("location:https://rasi-tech.herokuapp.com/");
 }
